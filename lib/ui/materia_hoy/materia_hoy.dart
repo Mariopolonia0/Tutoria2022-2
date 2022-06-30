bb916be009622d7b3149f94fcf8526e1dfcdc4c4
Mario <mariopolonia0@gmail.com>
2022-06-30 09:46:13 -0400

    klk

--- lib/ui/materia_hoy/materia_hoy.dart
+++ lib/ui/materia_hoy/materia_hoy.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:projecto_ucne/models/estudiante.dart';
-import '../../models/login.dart';
 
 class MateriaHoy extends StatelessWidget {
   MateriaHoy({super.key});
