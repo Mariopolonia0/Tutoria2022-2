c8ee69cf813bb3c7fefc259beb1e4eb728fbc796
Mario <mariopolonia0@gmail.com>
2022-07-30 11:14:57 -0400

    ventana estado lista para enlazar con api

--- lib/ui/estado_cuenta/estado_cuenta.dart
+++ lib/ui/estado_cuenta/estado_cuenta.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:projecto_ucne/models/Dto/estado_dto.dart';
 import '../../models/Dto/login_dto.dart';
+import 'package:intl/intl.dart';
 
 class EstadoCuenta extends StatefulWidget {
   const EstadoCuenta({super.key});
@@ -14,22 +16,13 @@ class _EstadoCuentaState extends State<EstadoCuenta> {
 
   int loading = 1;
 
+  String balance = '0.00';
+
   LoginDto arguments =
       LoginDto(estudianteId: 1, nombreEstudiante: 'Mario', matricula: '');
 
   //List<EstadoDto> _Estados = List.empty(growable: true);
-  List<EstadoDto> _Estados = [
-    EstadoDto(
-        descripcion: 'hola',
-        fechaRealizado: '22/10/2018',
-        transaccion: 9000.0,
-        estado: 25000),
-    EstadoDto(
-        descripcion: 'Adios',
-        fechaRealizado: '22/10/2019',
-        transaccion: 9000.0,
-        estado: 25000),
-  ];
+  List<EstadoDto> _estados = List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
@@ -66,8 +59,8 @@ class _EstadoCuentaState extends State<EstadoCuenta> {
   Widget getEstado() {
     return Column(
       children: [
-        //getSubTitulo(),
-        listaMateria()
+        getSubTitulo(),
+        listaMateria(),
       ],
     );
   }
@@ -87,12 +80,15 @@ class _EstadoCuentaState extends State<EstadoCuenta> {
                 Radius.circular(15),
               ),
             ),
-            child: getItemMateria(_Estados[index]),
+            child: getItemMateria(_estados[index]),
           )),
     );
   }
 
   Row getItemMateria(EstadoDto estadoDto) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {
+          balance = formatearMonto(estadoDto.estado);
+        }));
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -100,28 +96,38 @@ class _EstadoCuentaState extends State<EstadoCuenta> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             texto(estadoDto.descripcion),
-            //texto(formatoDinero(estadoDto.transaccion), color: const Color(0xFFEC1C24)),
+            (estadoDto.transaccion > 0.0)
+                ? texto(formatearMonto(estadoDto.transaccion),
+                    color: const Color(0xFF37B34A))
+                : texto(formatearMonto(estadoDto.transaccion),
+                    color: const Color(0xFFEC1C24))
           ],
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             texto(estadoDto.fechaRealizado),
-            //texto(formatoDinero(estadoDto.estado)),
+            texto(formatearMonto(estadoDto.estado))
           ],
         ),
       ],
     );
   }
 
-  
-
   Padding texto(String texto, {Color color = Colors.black}) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
       child: Text(texto,
-          style: TextStyle(fontWeight: FontWeight.bold, color: color)),
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Algerian',
+              color: color)),
     );
+  }
+
+  String formatearMonto(double texto) {
+    return "\$ ${NumberFormat("#,###.0#", "es_US").format(texto)}";
   }
 
   // octenerMaterias() async {
@@ -144,9 +150,10 @@ class _EstadoCuentaState extends State<EstadoCuenta> {
       alignment: Alignment.centerLeft,
       padding: const EdgeInsets.all(12),
       color: const Color(0xFF00247D),
-      child: const Text(
-        'Balance Generar RD\$ 13,000.0',
-        style: TextStyle(
+      child: Text(
+        'Balance Generar RD\$ $balance',
+        key: const Key("textKey"),
+        style: const TextStyle(
             fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
       ),
     );
